34a46fbf44ad22f8fdc0d4df95f21604499e156f
Anton Relin <arelin@sas.upenn.edu>
2016-05-07 15:13:21 -0400

    testing the button

--- app/src/main/java/com/example/arelin/yarr/MainActivity.java
+++ app/src/main/java/com/example/arelin/yarr/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.arelin.yarr;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button clickButton = (Button) findViewById(R.id.Listening);
+        clickButton.setOnClickListener( new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                System.out.print("test");
+
+            }
+        });
     }
+
 }
